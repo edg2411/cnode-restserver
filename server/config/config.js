c0cfa1d1a724c5c81321b67ee41a687c22994110
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-18 23:47:07 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://edg2411:VUs5UFbfCEiNL5SS@cluster0-y7edp.mongodb.net/cafe';
+    // urlDB = 'mongodb+srv://edg2411:VUs5UFbfCEiNL5SS@cluster0-y7edp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
